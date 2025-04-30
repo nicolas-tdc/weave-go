5ef42f18e680cfe849c734943028081c2f22400b
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-04-30 13:35:16 +0200

    Indentation main.go fix

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello from Weave-Go API!")
+	fmt.Fprintf(w, "Hello from Weave-Go API!")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    fmt.Println("Starting Weave-Go API on port 8080...")
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	fmt.Println("Starting Weave-Go API on port 8080...")
+	http.ListenAndServe(":8080", nil)
 }
